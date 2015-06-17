a3224190bf0f7f6cb4175e31cc18094662eb67b0
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2015-06-17 16:28:14 -0400

    lib: Initial implementation

---
+++ test/tracktics-mixpanel.test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var trackticsMixpanel = require('..');
+
+describe('Tracktics KISSmetrics Plugin', function() {
+    var subject;
+
+    beforeEach(function() {
+        global.window.mixpanel = jasmine.createSpyObj('mixpanel', ['track']);
+        subject = trackticsMixpanel();
+    });
+
+    describe('#trackPage()', function() {
+        var url;
+
+        beforeEach(function() {
+            url = '/some/url';
+            subject.trackPage(url);
+        });
+
+        it('should call mixpanel.track with expected args', function() {
+            expect(global.window.mixpanel.track).toHaveBeenCalledWith('Page Viewed', { page: url });
+        });
+    });
+
+    describe('#trackEvent()', function() {
+        var action, properties;
+
+        beforeEach(function() {
+            action = 'click';
+            properties = { event: 'Download' };
+        });
+
+        describe('when no additional properties are specified', function() {
+
+            beforeEach(function() {
+                subject.trackEvent(action, properties);
+            });
+
+            it('should call mixpanel.track with expected args', function() {
+                expect(global.window.mixpanel.track).toHaveBeenCalledWith(properties.event);
+            });
+        });
+
+        describe('when additional properties are specified', function() {
+
+            beforeEach(function() {
+                properties.plan = 'Pro';
+                subject.trackEvent(action, properties);
+            });
+
+            it('should call mixpanel.track with expected args', function() {
+                expect(global.window.mixpanel.track).toHaveBeenCalledWith(properties.event, { plan: 'Pro' });
+            });
+        });
+    });
+});
